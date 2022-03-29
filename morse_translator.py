f59217424b66502a62fcb647a7d2cbd04ae4eb5a
A. Bullock <mondeendeguise@gmail.com>
2022-03-29 10:44:29 -0600

    Add files via upload

---
+++ morse_translator.py
@@ -0,0 +1,43 @@
+morse_code = {
+  'A' : '.-',
+  'B' : '-...',
+  'C' : '-.-.',
+  'D' : '-..',
+  'E' : '.',
+  'F' : '..-.',
+  'G' : '--.',
+  'H' : '....',
+  'I' : '..',
+  'J' : '.---',
+  'K' : '-.-',
+  'L' : '.-..',
+  'M' : '--',
+  'N' : '-.',
+  'O' : '---',
+  'P' : '.--.',
+  'Q' : '--.-',
+  'R' : '.-.',
+  'S' : '...',
+  'T' : '-',
+  'U' : '..-',
+  'V' : '...-',
+  'W' : '.--',
+  'X' : '-..-',
+  'Y' : '-.--',
+  'Z' : '--..',
+	' ' : '+'
+  }
+
+def translate(phrase):
+  out = ("")
+  for n in range(len(str(phrase))):
+    out += f"{morse_code[phrase[n].upper()]}/"
+  return out
+
+
+def main():
+  while True:
+      usr_phrase = input(":")
+      print(translate(usr_phrase))
+if __name__ == '__main__':
+  main()
